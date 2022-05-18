88d6aff0c5c5da81f874a9a3bf4d091aa982a692
Janne Kärki <janne.karki@helsinki.fi>
2022-05-18 13:06:59 +0300

    add front page

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///user"
+db = SQLAlchemy(app)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html") 
