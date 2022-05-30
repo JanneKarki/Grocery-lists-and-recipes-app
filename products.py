368ecb2cd2a8f015bec27a42e82d5b0a76335504
Janne Kärki <janne.karki@helsinki.fi>
2022-05-30 10:56:39 +0300

    Function to check if product is already in db

--- products.py
+++ products.py
@@ -7,9 +7,25 @@ def get_products():
     return products_list
 
 def add_product(name):
-    sql = """INSERT INTO products (name)
-                    VALUES (:name) RETURNING id"""
-    product_id = db.session.execute(sql, {"name":name})
-    db.session.commit()
 
-    return product_id
+    product = find_product(name)
+
+    if not product:
+        print("not product in the list")
+        sql = """INSERT INTO products (name)
+                        VALUES (:name) RETURNING id"""
+        product_id = db.session.execute(sql, {"name":name})
+        db.session.commit()
+
+        return product_id
+        
+    return product[0]   
+
+
+def find_product(name):
+    sql = """SELECT * FROM products WHERE name=:name"""
+
+    product = db.session.execute(sql, {"name":name}).fetchone()
+    print(product)
+    return product
+
