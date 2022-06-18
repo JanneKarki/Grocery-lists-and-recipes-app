6da7a83551cc7948f18f6e808935f28d38e3422d
Janne Kärki <janne.karki@helsinki.fi>
2022-06-18 22:28:29 +0300

    add and remove new line in basket and function fixed to save basket to list

--- lists.py
+++ lists.py
@@ -4,16 +4,16 @@ import products
 
 def add_to_grocery_list(lists_id, shop_list):
     listed_items = shop_list.split()
-    print(listed_items)
+    print(listed_items, "listed item")
 
     sql = "INSERT INTO shopping_list (lists_id, product_id, amount, unit) values (:lists_id, :product_id, :amount, :unit)"
 
     for i in range (0,len(listed_items), 3):
         print(listed_items[i])
 
-        product_id = products.find_product(listed_items[i])
-
-        db.session.execute(sql, {"lists_id": lists_id, "product_id":product_id[0], "amount": listed_items[i+1], "unit": listed_items[i+2]})
+        product_id = products.add_product(listed_items[i])
+        print(product_id, "product_id")
+        db.session.execute(sql, {"lists_id": lists_id, "product_id":product_id, "amount": listed_items[i+1], "unit": listed_items[i+2]})
 
     db.session.commit()
 
