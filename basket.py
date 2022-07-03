bdb88df586beac1f9ac7bf1d9b212f3b0ace930b
Janne Kärki <janne.karki@helsinki.fi>
2022-07-03 22:14:49 +0300

    code cleaning

--- basket.py
+++ basket.py
@@ -5,7 +5,18 @@ import lists
 
 def add_to_basket(user_id, product_list):
 
-    sql = "INSERT INTO basket (user_id, product_id, amount, unit) values (:user_id, :product_id, :amount, :unit )"
+    sql = """INSERT INTO basket (
+                        user_id,
+                        product_id,
+                        amount,
+                        unit
+                        )
+                        values (
+                            :user_id,
+                            :product_id,
+                            :amount,
+                            :unit )
+                            """
    
     for i in range(len(product_list)):
 
@@ -14,39 +25,47 @@ def add_to_basket(user_id, product_list):
         unit = product_list[i][2]
 
         if not find_from_basket(product_id):
-
             db.session.execute(sql, {"user_id": user_id, "product_id":product_id, "amount":amount, "unit": unit})
-
             db.session.commit()
 
 
 def empty_basket(user_id):
 
     sql = "DELETE FROM basket WHERE user_id=user_id"
-
     db.session.execute(sql, {"user_id":user_id})
-
     db.session.commit()
+
 
 def get_basket(user_id):
 
-    sql = "SELECT products.name, basket.amount, basket.unit FROM products, basket WHERE products.id = basket.product_id AND basket.user_id=user_id ORDER BY basket.id" 
+    sql = """SELECT 
+            products.name,
+            basket.amount,
+            basket.unit
+            FROM
+            products,
+            basket
+            WHERE
+            products.id = basket.product_id
+            AND
+            basket.user_id=user_id
+            ORDER BY
+            basket.id
+            """ 
 
     basket_list = db.session.execute(sql, {"user_id":user_id}).fetchall()
-
     return basket_list
+
 
 def add_recipe_to_basket(user_id, recipe_id):
 
     ingredients = recipes.get_recipe_ingredients(recipe_id)
-
     add_to_basket(user_id, ingredients)
     
 
 def add_list_to_basket(user_id, list_id):
 
     grocery_list = lists.get_grocery_list(list_id)
-
     add_to_basket(user_id, grocery_list)
     
 
@@ -59,33 +78,38 @@ def format_and_send_to_basket(user_id, products_string):
         product = product_list[i]
         amount = 0.0
         unit = "-"
-        
+
         formatted_list.append((product, amount, unit))
 
     add_to_basket(user_id, formatted_list)
 
-    
+
 def find_from_basket(id):
 
-    sql = "SELECT * FROM basket WHERE product_id =:id"
+    sql = """SELECT
+            id,
+            user_id,
+            product_id,
+            amount,
+            float
+            FROM
+            basket
+            WHERE
+            product_id =:id
+            """
 
     result = db.session.execute(sql, {"id":id}).fetchone()
-
     if not result:
         return False
-    
     return True
 
 def update_basket(user_id, product_list):
 
     formatted_list = []
-
     for i in range(0, len(product_list), 3):
-
         product = product_list[i]
         amount = product_list[i+1]
         unit = product_list[i+2]
-
         formatted_list.append((product,amount,unit))
 
     add_to_basket(user_id, formatted_list)
